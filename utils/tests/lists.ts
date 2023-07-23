c65f2aca140ec16e4db719af110c722cbd75e13a
AndreySkripachev <andrey-skripachev@saritasa.com>
2023-07-23 13:19:15 +0700

    Fix a bug in test file

--- utils/tests/lists.ts
+++ utils/tests/lists.ts
@@ -1,3 +1,5 @@
+import { randomInt } from './number';
+
 interface ListBase<T> {
   value: T | null;
   next: ListBase<T> | null;
@@ -7,10 +9,31 @@ const getListLength = <T, TList extends ListBase<T>>(listBase: TList): number =>
   let length = 0;
   let current: ListBase<T> | null = listBase;
 
-  while (current !== null) {
+  while (current !== null && current.value !== null) {
     length++;
     current = current.next;
   }
 
   return length;
+}
+
+export const getRandomListItem = <T, TList extends ListBase<T>>(listBase: TList): TList => {
+  const listLength = getListLength(listBase);
+  let from = 1;
+  let to = listLength - 1;
+
+  if (listLength < 3) {
+    from = 0;
+    to = listLength;
+  }
+
+  const randomIndex = randomInt(from, to);
+
+  let current = listBase;
+
+  for (let i=from; i < to; i++) {
+    current = current.next as TList;
+  }
+
+  return current;
 }
